24f6f9f7fec63749f9c0f822327a68094ece2320
CvH <namerp@googlemail.com>
2020-02-02 09:05:03 +0100

    disable modules

--- v4l/scripts/make_kconfig.pl
+++ v4l/scripts/make_kconfig.pl
@@ -626,6 +626,11 @@ open_kconfig('../linux', './Kconfig.staging');
 close OUT;
 
 # These options should default to off
+disable_config('MEDIA_ANALOG_TV_SUPPORT');
+disable_config('MEDIA_CAMERA_SUPPORT');
+disable_config('MEDIA_CEC_SUPPORT');
+disable_config('SOC_CAMERA');
+disable_config('CEC_CORE');
 disable_config('DVB_AV7110_FIRMWARE');
 disable_config('DVB_CINERGYT2_TUNING');
 disable_config('VIDEO_HELPER_CHIPS_AUTO');
